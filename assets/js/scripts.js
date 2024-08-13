2b59514d5596c13ddc0ef86454507ba37e79c3c2
csgutierm <csgutier@gmail.com>
2024-08-12 21:53:35 -0400

    Páginas de cursos, habilidades, repositorios, proyectos Se agregan links

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,26 @@
 // Código JavaScript aquí 
+/*
+document.addEventListener("DOMContentLoaded", function() {
+
+    //CONTENIDO MODULAR
+
+    // Selecciona el elemento donde se cargará el footer
+    const footerContainer = document.getElementById("footer");
+
+    // Utiliza fetch para obtener el contenido del footer desde footer.html
+    fetch("assets/modulos/footer.html")
+        .then(response => {
+            if (response.ok) {
+                return response.text();
+            } else {
+                throw new Error("No se pudo cargar el footer.");
+            }
+        })
+        .then(data => {
+            footerContainer.innerHTML = data;
+        })
+        .catch(error => {
+            console.error("Error al cargar el footer:", error);
+        });
+});
+*/
